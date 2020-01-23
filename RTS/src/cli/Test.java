ca8ea7e59422f91ff78d5cd99bb9635c1b83e661
Azilisiev <bordeneuve.awen@gmail.com>
2020-01-23 12:04:02 +0100

    un msg

--- RTS/src/cli/Test.java
+++ RTS/src/cli/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("T pas moche");
-		System.out.println("si");
 		//test 2
 	}
 
