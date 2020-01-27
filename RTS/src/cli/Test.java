6a7459904384601ba12b0898b17fa4925c185c86
JustAdel <adabbs2014@gmail.com>
2020-01-27 13:49:36 +0100

    :)

--- RTS/src/cli/Test.java
+++ RTS/src/cli/Test.java
@@ -3,10 +3,13 @@ package cli;
 public class Test {
 
 	public static void main(String[] args) {
+<<<<<<< Upstream, based on branch 'master' of https://github.com/JustAdel/L2-GLP
 		// TODO Auto-generated method stub
 		System.out.println("T pas moche");
 		//test 5
 		//modification
+=======
+>>>>>>> cda6309 :)
 
 	}
 
