711e0f047d238e37cf1c41c48e8cdc2f36a01185
JustAdel <adabbs2014@gmail.com>
2020-02-03 19:18:55 +0100

    update Selectable.java

--- RTS/src/cli/data/Selectable.java
+++ RTS/src/cli/data/Selectable.java
@@ -1,5 +1,47 @@
 package cli.data;
 
 public abstract class Selectable {
-
+	private int health;
+	private int maxHealth;
+	private boolean selected;
+	private String factionName;
+	private String name;
+	
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	public String getFactionName() {
+		return factionName;
+	}
+	public void setFactionName(String factionName) {
+		this.factionName = factionName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Selectable [health=" + health + ", maxHealth=" + maxHealth + ", selected=" + selected + ", factionName="
+				+ factionName + ", name=" + name + "]";
+	} 
+	
 }
