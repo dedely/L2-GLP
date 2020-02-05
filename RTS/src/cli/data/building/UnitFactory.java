42cf1af14b014ebd8ab5ea479b7f0cd935b678b9
Adel <adabbs2014@gmail.com>
2020-02-05 09:50:50 +0100

    update UnitFactory

--- RTS/src/cli/data/building/UnitFactory.java
+++ RTS/src/cli/data/building/UnitFactory.java
@@ -1,5 +1,13 @@
 package cli.data.building;
 
+import cli.data.Coordinates;
+import cli.data.Description;
+
 public abstract class UnitFactory extends Building{
+
+	public UnitFactory(int health, int maxHealth, boolean selected, String factionName, String name,
+			Coordinates position, Description description, Coordinates rallyPoint) {
+		super(health, maxHealth, selected, factionName, name, position, description, rallyPoint);
+	}
 	
 }
