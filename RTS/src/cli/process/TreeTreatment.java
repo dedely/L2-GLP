f973ebe02d29bc491a83a58a19748a15c84d578a
maria-byte <marialorenapoupet@yahoo.fr>
2020-02-20 17:24:51 +0100

    Creation class TreeTreamtment

--- RTS/src/cli/process/TreeTreatment.java
+++ RTS/src/cli/process/TreeTreatment.java
@@ -5,7 +5,7 @@ import cli.data.faction.Faction;
 public class TreeTreatment {
 
 	public void startResearch (Faction faction) {
-		if (faction.getResources())
+		
 	}
 	
 	public void isResearching (Faction faction) {
