74e35cd5c563a03673e19f38832ef90e0df7e388
maria-byte <marialorenapoupet@yahoo.fr>
2020-02-20 16:57:55 +0100

    test commit

--- RTS/src/cli/process/TreeTreatment.java
+++ RTS/src/cli/process/TreeTreatment.java
@@ -1,5 +1,15 @@
 package cli.process;
 
+import cli.data.faction.Faction;
+
 public class TreeTreatment {
 
+	public void startResearch (Faction faction) {
+		if (faction.getResources())
+	}
+	
+	public void isResearching (Faction faction) {
+		if (startResearch)
+	}
+	
 }
