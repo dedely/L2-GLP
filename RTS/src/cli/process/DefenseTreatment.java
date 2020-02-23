b89f629ebc94e67d794e699824673b0d6105300e
maria-byte <marialorenapoupet@yahoo.fr>
2020-02-23 20:51:31 +0100

    Changed methods

--- RTS/src/cli/process/DefenseTreatment.java
+++ RTS/src/cli/process/DefenseTreatment.java
@@ -1,6 +1,7 @@
 package cli.process;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import cli.data.Constants;
 import cli.data.Coordinates;
@@ -10,27 +11,40 @@ import cli.data.unit.Unit;
 
 public class DefenseTreatment {
 
-	public Selectable detectClosestEnnemy (Unit caster) {
-			Selectable closestEnnemy = CoordinatesTreatment.getClosestSelectable;
-			return closestEnnemy;
+	public Selectable detectClosestEnnemy (Unit caster, HashMap<Coordinates, Selectable> positions) {
+		Selectable closestEnnemy = null;
+		if (caster.isCanShootTargetsOnDifferentHeight()) {
+			closestEnnemy = CoordinatesTreatment.closestSelectable(caster, positions);
+		}
+		else {
+			closestEnnemy = CoordinatesTreatment.closestGroundSelectable(caster, positions);
+			}
+		return closestEnnemy;
+
 		}
 	
-	public Selectable detectClosestEnnemy (DefenseBuilding caster) {
+	public Selectable detectClosestEnnemy (DefenseBuilding caster, HashMap<Coordinates, Selectable> positions) {
 		Selectable closestEnnemy = null;
+		if (CoordinatesTreatment.closestSelectable(caster, positions) != null) {
+			closestEnnemy = CoordinatesTreatment.closestSelectable(caster, positions);
+		}
+		else {}
 		return closestEnnemy;
 	}
 
-	public void defense (Unit caster) {
-		if (detectClosestEnnemy(caster) != null) {
-			AttackTreatment.attack(caster, detectClosestEnnemy(caster));
+	public void defense (Unit caster, HashMap<Coordinates, Selectable> positions) {
+		if (detectClosestEnnemy(caster, positions) != null) {
+			AttackTreatment.attack(caster, detectClosestEnnemy(caster, positions));
 		}
+		else {}
 	}
 	
 	
-	public void defense (DefenseBuilding caster) {
-		if (detectClosestEnnemy(caster) != null) {
-			AttackTreatment.attack(caster, detectClosestEnnemy(caster));
+	public void defense (DefenseBuilding caster, HashMap<Coordinates, Selectable> positions) {
+		if (detectClosestEnnemy(caster, positions) != null) {
+			AttackTreatment.attack(caster, detectClosestEnnemy(caster, positions));
 		}
+		else {}
 	}
 	
 }
