a516cdb4745b3dfbc5f3da8501c798e7f075f065
Adel <adabbs2014@gmail.com>
2020-02-23 20:29:00 +0100

    useless update

---
+++ RTS/src/cli/process/UnitBuildingFactory.java
@@ -0,0 +1,17 @@
+package cli.process;
+
+import cli.data.Coordinates;
+import cli.data.building.Building;
+
+public class UnitBuildingFactory implements BuildingFactory{
+
+	@Override
+	public Building createBuilding(String factionName, String type, Coordinates position) {
+		
+		
+		
+		
+		return null;
+	}
+
+}
