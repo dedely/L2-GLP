d5a8e8f38d99d2acd80ad90bc2488cb82216fe0d
Azilisiev <bordeneuve.awen@gmail.com>
2020-02-25 12:56:11 +0100

    created a Engine class

---
+++ RTS/src/cli/process/Engine.java
@@ -0,0 +1,44 @@
+package cli.process;
+
+public class Engine {
+	
+	public static void main(String args[]) {
+		boolean NoErrorsCondition = true;
+		boolean NoStopCondition = true;
+		
+		while(NoErrorsCondition && NoStopCondition ) {
+			
+			/*inputs treatment*/
+			
+			/*updating the game*/
+			
+			/*generate outputs*/
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
