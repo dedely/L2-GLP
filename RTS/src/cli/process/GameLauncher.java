9c58cb041fdee6f5bf1d139a29601890369c9d60
Adel <adabbs2014@gmail.com>
2020-03-01 12:09:47 +0100

    test

--- RTS/src/cli/process/GameLauncher.java
+++ RTS/src/cli/process/GameLauncher.java
@@ -1,21 +1,17 @@
 package cli.process;
 
-import java.util.ArrayList;
-
 import cli.data.Config;
 import cli.data.Constants;
 import cli.data.GameState;
-import cli.data.faction.Faction;
 import cli.data.map.Map;
 import cli.process.builder.MapBuilder;
 import cli.process.factory.FactionFactory;
 import cli.tests.input.InputParameter;
 
 public class GameLauncher {
-	
+
 	private Config config;
-	
-	
+
 	public GameLauncher(Config config) {
 		this.config = config;
 	}
@@ -24,17 +20,13 @@ public class GameLauncher {
 		MapBuilder builder = new MapBuilder();
 		Map map = builder.buildMap(InputParameter.MAP_PATH);
 		GameState state = new GameState(map);
+
 		initializeFactions(state);
 	}
 
 	private void initializeFactions(GameState state) {
 		state.getFactions().add(FactionFactory.createFaction(config.getPlayerFactionName(), Constants.PLAYER));
-		
+
 	}
-	
-	
-	
-	
-	
-	
+
 }
