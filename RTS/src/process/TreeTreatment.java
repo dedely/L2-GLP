1dd56296c5ee889c14c96a35565fe6a202740c65
maria-byte <marialorenapoupet@yahoo.fr>
2020-03-04 13:21:22 +0100

    ResearchTreeBuilder modificated

--- RTS/src/process/TreeTreatment.java
+++ RTS/src/process/TreeTreatment.java
@@ -1,24 +1,30 @@
 package process;
 
 import data.Constants;
-import data.building.ResearchBuilding;
 import data.faction.Faction;
-import data.tree.BinaryTree;
 
 public class TreeTreatment {
-
-	/*
-	 * 
-	 *
-	 * public Boolean startResearch (Faction faction, int indice) { if
-	 * (faction.getCurrentResearch().equals(false)){ for(int i = 0 ; i <
-	 * faction.getBuildingList().size(); i++) { if
-	 * (faction.getBuildingList().get(i).getName() == Laboratory ) { if
-	 * (faction.getResearchTree().getResearches().get(indice).isUnlocked() == true)
-	 * { if(faction.getResources().get(1).getResourceCount() >=
-	 * faction.getResearchTree().getResearches().get(indice).getCost()) {
-	 * faction.getResources().get(1).setResourceCount(faction.getResources().get(1).
-	 * getResourceCount()-faction.getResearchTree().getResearches().get(indice).
-	 * getCost()); faction.setCurrentResearch(true); // Lance le timer. } } } } } }
-	 */
+		 	 
+	  public Boolean startResearch (Faction faction, int indice) { 
+		  if (faction.getCurrentResearch().equals(false)){ 
+			  for(int i = 0 ; i <faction.getBuildingList().size(); i++) { 
+				  if(faction.getBuildingList().get(i).getName() == Constants.LABORATORY ) { 
+					  if(faction.getResearchTree().getResearches().get(indice).isUnlocked() == true){
+						  if(faction.getResources().get(1).getResourceCount() >=faction.getResearchTree().getResearches().get(indice).getCost()) {
+							  	faction.getResources().get(1).setResourceCount(faction.getResources().get(1).
+							  	getResourceCount()-faction.getResearchTree().getResearches().get(indice).
+							  	getCost()); faction.setCurrentResearch(true); 
+							  	// Lance le timer. 
+						  }
+					  }
+				  }
+			  }
+		return true;
+		}
+		return false;
+	}
+	 
+	  public void stopResearch (Faction faction, int indice) {
+		  faction.setCurrentResearch(false);
+	  }
 }
--- RTS/src/process/builder/ResearchTreeBuilder.java
+++ RTS/src/process/builder/ResearchTreeBuilder.java
@@ -1,22 +1,89 @@
 package process.builder;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import data.Constants;
 import data.faction.Faction;
-import data.tree.BinaryTree;
 import data.tree.Research;
+import data.tree.Upgrade;
 
 public class ResearchTreeBuilder {
 
-	private String faction;
-
+	public String faction;
+	public static final String SEPARATOR = ";";
+	
 	public ResearchTreeBuilder(String faction) {
 		this.faction = faction;
 	}
 
-	/*
-	 * public BinaryTree BuildTree (Faction faction) { BinaryTree tree = new
-	 * BinaryTree (); for (Research res: tree) {
-	 * 
-	 * } }
-	 */
-
+	
+	  public void BuildTree (Faction faction, String fileName) throws IOException {
+		  Map<Integer, Research> treeMap = faction.getResearchTree().getResearches();
+		  for (Map.Entry<Integer, Research> entry : treeMap.entrySet()) {
+			String line, fields[];
+			
+			try { 
+					if (faction.getName().equals(Constants.UNION)) {
+						BufferedReader upgradesFileUnion = new BufferedReader(new FileReader(fileName));
+						while ((line = upgradesFileUnion.readLine()) != null) {
+							fields = line.split(SEPARATOR);
+							for (String s : fields) {
+								String name = " ";
+								int value = -1;
+								String elements[];
+								elements = s.split(",");
+								name = elements[0];
+								value = Integer.parseInt(elements[1]);
+								addToResearch(entry.getValue(), new Upgrade(name, value));
+							}
+							upgradesFileUnion.close();
+						}
+					}
+						else if (faction.getName().equals(Constants.REPUBLIC)) {
+							BufferedReader upgradesFileRepublic = new BufferedReader(new FileReader(fileName));
+							while ((line = upgradesFileRepublic.readLine()) != null) {
+								fields = line.split(SEPARATOR);
+								for (String s : fields) {
+									String name = " ";
+									int value = -1;
+									String elements[];
+									elements = s.split(",");
+									name = elements[0];
+									value = Integer.parseInt(elements[1]);
+									addToResearch(entry.getValue(), new Upgrade(name, value));
+								}
+								upgradesFileRepublic.close();
+							}
+						}
+							else if (faction.getName().equals(Constants.FEDERATION)) {
+								BufferedReader upgradesFileFederation = new BufferedReader(new FileReader(fileName));
+								while ((line = upgradesFileFederation.readLine()) != null) {
+									fields = line.split(SEPARATOR);
+									for (String s : fields) {
+										String name = " ";
+										int value = -1;
+										String elements[];
+										elements = s.split(",");
+										name = elements[0];
+										value = Integer.parseInt(elements[1]);
+										addToResearch(entry.getValue(), new Upgrade(name, value));
+									}
+									upgradesFileFederation.close();
+							}
+						
+			}
+			}catch (FileNotFoundException e) {
+				System.err.println(e.getMessage());
+			}catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		  }
+	  }
+	  
+	  public void addToResearch (Research research, Upgrade upgrade) {
+		  research.getUpgrades().add(upgrade);
+	  }
 }
