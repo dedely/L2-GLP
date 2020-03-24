e95ea051f48993a9c292a36ea3ee4d91bd4c132c
Azilisiev <bordeneuve.awen@gmail.com>
2020-03-24 23:18:15 +0100

    added some orders

--- RTS/src/process/OrderTreatment.java
+++ RTS/src/process/OrderTreatment.java
@@ -10,6 +10,7 @@ import data.order.Order;
 import data.unit.Unit;
 import data.order.Attack;
 import data.order.AttackPosition;
+import data.order.Embark;
 import data.order.MoveToPosition;
 import data.order.MoveToTarget;
 import data.Coordinates;
@@ -34,23 +35,32 @@ public class OrderTreatment {
 	public static void finishOrder(Selectable orderReceiver) {
 		ArrayList<Order> orderList = orderReceiver.getOrders();
 		orderList.remove(0);
-		//System.out.println("\norder complete\n");
+		// System.out.println("\norder complete\n");
 
 	}
 
 	public void giveOrdre(String type, Unit receiver, Selectable target) {
+		SelectableRepository repository = SelectableRepository.getInstance();
 		switch (type) {
 		case Constants.ATTACK_STS:
-			giveOrderReplace(receiver, new Attack(Constants.STOP_TO_SHOOT, target));
+			giveOrderReplace(receiver,
+					new Attack(Constants.STOP_TO_SHOOT, repository.getSelectable(target.getPosition())));
 			break;
 		case Constants.ATTACK_POS_AAC:
-			giveOrderReplace(receiver, new Attack(Constants.GO_AT_ALL_COST, target));
+			giveOrderReplace(receiver,
+					new Attack(Constants.GO_AT_ALL_COST, repository.getSelectable(target.getPosition())));
 			break;
 		case Constants.MOVE_TO_POSITION_STS:
 			giveOrderReplace(receiver, new MoveToTarget(target, Constants.STOP_TO_SHOOT));
 			break;
 		case Constants.MOVE_TO_POSITION_AAC:
 			giveOrderReplace(receiver, new MoveToTarget(target, Constants.GO_AT_ALL_COST));
+			break;
+		case Constants.EMBARK_STS:
+			giveOrderReplace(receiver, new Embark(target, Constants.STOP_TO_SHOOT));
+			break;
+		case Constants.EMBARK_AAC:
+			giveOrderReplace(receiver, new Embark(target, Constants.GO_AT_ALL_COST));
 			break;
 		}
 	}
@@ -88,9 +98,8 @@ public class OrderTreatment {
 	public static void executeNextOrder(Unit executor) {
 		Order currentOrder = nextOrder(executor);
 		if (currentOrder == null) {
-			//System.out.println("no order");
-		}
-		else {
+			// System.out.println("no order");
+		} else {
 			Coordinates destination = null;
 			Coordinates actualPosition = null;
 
@@ -121,30 +130,79 @@ public class OrderTreatment {
 					} else
 						finishOrder(executor);
 				}
+			case Constants.MOVE_TO_TARGET_AAC:
+				destination = ((MoveToPosition) currentOrder).getPosition();
+				actualPosition = executor.getPosition();
+				if (!destination.equals(actualPosition)) {
+					MoveToTreatment.moveToward(executor, destination);
+				} else {
+					finishOrder(executor);
+				}
+				break;
+			case Constants.MOVE_TO_TARGET_STS:
+				if (hasEnemyInRange(executor)) {
+					if (SelectableTreatment.canShoot(executor,
+							CoordinatesTreatment.closestEnnemySelectable(executor))) {
+						SelectableTreatment.dealDamage(executor,
+								CoordinatesTreatment.closestEnnemySelectable(executor));
+						executor.getWeapon().setTimeLeftToReload(executor.getWeapon().getTimeBeetweenShots());
+					}
+				} else {
+					destination = ((MoveToPosition) currentOrder).getPosition();
+					actualPosition = executor.getPosition();
+					if (!destination.equals(actualPosition)) {
+						MoveToTreatment.moveToward(executor, destination);
+					} else {
+						finishOrder(executor);
+					}
 
+				}
+				break;
+			case Constants.ATTACK_STS:
+
+				Selectable target = ((Attack) currentOrder).getTarget();
+				if (target != null) {
+					if (target.getCurrentHealth() > 0) {
+
+						AttackTreatment.attack(executor, target);
+					}
+				} else {
+					System.out.println("unit is null");
+					finishOrder(executor);
+				}
 			}
 		}
-
 	}
 
 	public static boolean hasEnemyInRange(Unit unit) {
 		if (CoordinatesTreatment.closestEnnemySelectable(unit) != null) {
 			return true;
-		} else
+		} else {
 			return false;
-
+		}
 	}
 
 	public static Order nextOrder(Selectable orderReceiver) {
 		if (!orderReceiver.getOrders().isEmpty()) {
 			return orderReceiver.getOrders().get(0);
-		} else
+		} else {
 			return null;
+		}
 	}
 
 	public static void executeNextOrder(Building currentBuilding) {
-		// TODO Auto-generated method stub
+		Order currentOrder = nextOrder(currentBuilding);
+		if (currentOrder == null) {
+			// System.out.println("no order");
+		} else {
+			Coordinates destination = null;
+			Coordinates actualPosition = null;
+
+			switch (currentOrder.getType()) {
+
+			}
+
+		}
 
 	}
-
 }
--- RTS/src/process/factory/UnitFactory.java
+++ RTS/src/process/factory/UnitFactory.java
@@ -4,13 +4,18 @@ import data.Constants;
 import data.Coordinates;
 import data.faction.Faction;
 import data.unit.GroundUnit;
+import data.unit.TroopTransport;
 import data.unit.Unit;
 
 public class UnitFactory {
 	public static Unit createUnit(String type, Coordinates spawnPosition, Faction faction) throws IllegalArgumentException {
-		if (type == Constants.TEST_GROUND) {
+		switch (type) {
+		case Constants.TEST_GROUND:
 			return new GroundUnit("Test ground unit", 500, faction, 500, 0, Constants.SUPER_LIGHT,
-					"A test unit", spawnPosition, 5, WeaponFactory.createWeapon(Constants.TEST_GROUND_WEAPON_H), 10);
+					"A test unit", spawnPosition, 2, WeaponFactory.createWeapon(Constants.TEST_GROUND_WEAPON_H), 10);
+		case Constants.TEST_GROUND_EMBARK:
+			return new TroopTransport("test transport", 1000, faction, 1000, 0, Constants.LIGHT, "A test unit", spawnPosition, 2, WeaponFactory.createWeapon(Constants.TEST_GROUND_WEAPON_H), 5, 5);
+		
 		}
 
 		throw new IllegalArgumentException("type " + type + " is not implemented or defined");
