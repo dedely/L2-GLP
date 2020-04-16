3ca40534636b781a1d3cb2deb3019f319dcf01f0
Azilisiev <bordeneuve.awen@gmail.com>
2020-04-16 16:53:36 +0200

    try 1 to make title screen and options

--- RTS/src/gui/elements/TitleScreen.java
+++ RTS/src/gui/elements/TitleScreen.java
@@ -1,27 +1,44 @@
 package gui.elements;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import data.Config;
 import data.Constants;
+import data.Player;
 import process.Game;
 
 /**
- * @author Adel
+ * @author Awen
  *
  */
 public class TitleScreen extends Dashboard {
 
-	private JLabel titleLabel = new JLabel("War Never Dies");
+	private JLabel titleLabel = new JLabel("War Never Dies", SwingConstants.CENTER);
 	private JButton playButton = new JButton("New game");
-	
+	private JButton quitButton = new JButton("Quit game");
+	private JPanel optionPanel = new JPanel();
+	private JLabel nameLabel = new JLabel("Your name", SwingConstants.CENTER);
+	private JTextField nameField = new JTextField();
+	private JLabel optionsLabel = new JLabel("Select your faction", SwingConstants.CENTER);
+	private JButton union = new JButton("Union");
+	private JButton federation = new JButton("Federation");
+	private JButton republic = new JButton("Republic");
+	private JButton back = new JButton("Go Back to Title Screen");
+
 	private int state;
-	
+
 	private Config config;
 
 	public TitleScreen(Game game) {
@@ -32,22 +49,90 @@ public class TitleScreen extends Dashboard {
 	}
 
 	private void initLayout() {
-		setLayout(new BorderLayout());
-		add(BorderLayout.NORTH, titleLabel);
-		add(BorderLayout.CENTER, playButton);
+		Dimension dimensionButtons = new Dimension(100, 100);
+		playButton.setMinimumSize(dimensionButtons);
+		quitButton.setMinimumSize(dimensionButtons);
+		setLayout(new GridLayout(3, 1));
+		add(titleLabel);
+		add(playButton);
+		add(quitButton);
 	}
 
 	private void initActions() {
 		playButton.addActionListener(new PlayAction());
+		quitButton.addActionListener(new QuitAction());
+		union.addActionListener(new UnionChoosen());
+		federation.addActionListener(new FederationChoosen());
+		republic.addActionListener(new RepublicChoosen());
 
+	}
+
+	private void showOptions() {
+		removeAll();
+		optionPanel.add(nameLabel);
+		optionPanel.add(nameField);
+		optionPanel.add(optionsLabel);
+		optionPanel.add(union);
+		optionPanel.add(federation);
+		optionPanel.add(republic);
+		optionPanel.add(back);
+		add(optionPanel);
+		state++;
+	
 	}
 
 	private class PlayAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			getGame().launch(Constants.DEFAULT_CONFIG);
+			//getGame().launch(Constants.DEFAULT_CONFIG);
+			showOptions();
+		}
 
+	}
+
+	private class QuitAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+
+		}
+
+	}
+
+	private class UnionChoosen implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			ArrayList<Player> players = new ArrayList<Player>();
+			Player player = new Player(nameField.getText(), "Union");
+			players.add(player);
+			config.setPlayers(players);
+		}
+
+	}
+	
+	private class FederationChoosen implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			ArrayList<Player> players = new ArrayList<Player>();
+			Player player = new Player(nameField.getText(), "Federation");
+			players.add(player);
+			config.setPlayers(players);
+		}
+
+	}
+	
+	private class RepublicChoosen implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			ArrayList<Player> players = new ArrayList<Player>();
+			Player player = new Player(nameField.getText(), "Republic");
+			players.add(player);
+			config.setPlayers(players);
 		}
 
 	}
